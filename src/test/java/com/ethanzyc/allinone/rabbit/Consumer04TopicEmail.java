98bc2efe31ba44e29a4fb985bc2d7d91ad48c574
zhuyc <zhuyuchen321@163.com>
2019-06-29 00:03:48 +0800

    topic模式

---
+++ src/test/java/com/ethanzyc/allinone/rabbit/Consumer04TopicEmail.java
@@ -0,0 +1,89 @@
+package com.ethanzyc.allinone.rabbit;
+
+import com.rabbitmq.client.*;
+
+import java.io.IOException;
+import java.util.concurrent.TimeoutException;
+
+/**
+ * @author ethan
+ * @date 2019/6/27 23:30
+ */
+public class Consumer04TopicEmail {
+
+
+    public static final String QUEUE_INFORM_EMAIL = "queue_inform_email";
+    public static final String EXCHANGE_TOPIC_INFORM = "exchange_topic_inform";
+    public static final String ROUTING_EMAIL = "inform.#.email.#";
+
+    public static void main(String[] args) {
+        // 消费者与mq建立连接
+        ConnectionFactory factory = new ConnectionFactory();
+        factory.setHost("127.0.0.1");
+        factory.setPort(5673);
+        factory.setUsername("guest");
+        factory.setPassword("guest");
+        factory.setVirtualHost("/");
+
+        Connection connection = null;
+        try {
+            connection = factory.newConnection();
+            Channel channel = connection.createChannel();
+
+            channel.queueDeclare(QUEUE_INFORM_EMAIL, true, false, false, null);
+            /**
+             * String exchange, String type
+             * exchange:名称
+             * type:交换机类型，
+             *  fanout:对应rabbit的工作模式是 publish/subscribe
+             *  direct:对应rabbit的工作模式是 routing
+             *  topic: 对应rabbit的工作模式是 topics
+             *  headers: 对应rabbit的工作模式是 headers
+             */
+            channel.exchangeDeclare(EXCHANGE_TOPIC_INFORM, BuiltinExchangeType.TOPIC);
+
+            // 交换机和队列进行绑定
+            /**
+             * String queue, String exchange, String routingKey
+             * queue: 队列名称
+             * exchange: 交换机名称
+             * routingKey: 路由key，作用是交换机根据路由key的值将消息转发到指定到队列中，在发布订阅中协调为空字符串
+             */
+            channel.queueBind(QUEUE_INFORM_EMAIL, EXCHANGE_TOPIC_INFORM, ROUTING_EMAIL);
+
+            DefaultConsumer consumer = new DefaultConsumer(channel) {
+                // 当接受到消息，此方法会被调用
+
+                /**
+                 *
+                 * @param consumerTag 消费者标签，用来标识消费者，在监听队列到时候可设置
+                 * @param envelope 信封
+                 * @param properties
+                 * @param body
+                 * @throws IOException
+                 */
+                @Override
+                public void handleDelivery(String consumerTag, Envelope envelope, AMQP.BasicProperties properties, byte[] body) throws IOException {
+                    // 交换机
+                    String exchange = envelope.getExchange();
+                    System.out.println("exchange:" + exchange);
+                    // 消息id，用来标识消息，保证消息送达
+                    long deliveryTag = envelope.getDeliveryTag();
+                    System.out.println("deliveryTag:" + deliveryTag);
+                    String s = new String(body, "utf-8");
+                    System.out.println("收到的消息" + s);
+                }
+            };
+
+
+            String basicConsume = channel.basicConsume(QUEUE_INFORM_EMAIL, true, consumer);
+            System.out.println("basicConsume:" + basicConsume);
+
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        } catch (TimeoutException e) {
+            e.printStackTrace();
+        }
+    }
+}
---
+++ src/test/java/com/ethanzyc/allinone/rabbit/Producer04Topic.java
@@ -0,0 +1,96 @@
+package com.ethanzyc.allinone.rabbit;
+
+import com.rabbitmq.client.BuiltinExchangeType;
+import com.rabbitmq.client.Channel;
+import com.rabbitmq.client.Connection;
+import com.rabbitmq.client.ConnectionFactory;
+
+import java.io.IOException;
+import java.util.concurrent.TimeoutException;
+
+/**
+ * @author ethan
+ * @date 2019/6/27 22:58
+ */
+public class Producer04Topic {
+
+    public static final String QUEUE_INFORM_EMAIL = "queue_inform_email";
+    public static final String QUEUE_INFORM_SMS = "queue_inform_sms";
+    public static final String EXCHANGE_TOPIC_INFORM = "exchange_topic_inform";
+    public static final String ROUTING_EMAIL = "inform.#.email.#";
+    public static final String ROUTING_SMS = "inform.#.sms.#";
+
+    public static void main(String[] args) {
+        // 生产者与mq建立连接
+        ConnectionFactory factory = new ConnectionFactory();
+        factory.setHost("127.0.0.1");
+        factory.setPort(5673);
+        factory.setUsername("guest");
+        factory.setPassword("guest");
+        factory.setVirtualHost("/");
+
+        Connection connection = null;
+        Channel channel = null;
+        try {
+            connection = factory.newConnection();
+            channel = connection.createChannel();
+            /**
+             * String queue, boolean durable, boolean exclusive, boolean autoDelete, Map<String, Object> arguments
+             * queue:队列名称
+             * durable:是否持久：重启仍在
+             * exclusive:是否独占连接，只在该连接中可以访问。连接关闭，队列删除
+             * autoDelete:是否自动删除，独占和自动删除设为true可用于临时队列
+             * arguments:设置拓展参数：比如连接时间
+             */
+
+            channel.queueDeclare(QUEUE_INFORM_EMAIL, true, false, false, null);
+            channel.queueDeclare(QUEUE_INFORM_SMS, true, false, false, null);
+            /**
+             * String exchange, String type
+             * exchange:名称
+             * type:交换机类型，
+             *  fanout:对应rabbit的工作模式是 publish/subscribe
+             *  direct:对应rabbit的工作模式是 routing
+             *  topic: 对应rabbit的工作模式是 topics
+             *  headers: 对应rabbit的工作模式是 headers
+             */
+            channel.exchangeDeclare(EXCHANGE_TOPIC_INFORM, BuiltinExchangeType.TOPIC);
+
+            // 交换机和队列进行绑定
+            /**
+             * String queue, String exchange, String routingKey
+             * queue: 队列名称
+             * exchange: 交换机名称
+             * routingKey: 路由key，作用是交换机根据路由key的值将消息转发到指定到队列中，在发布订阅中协调为空字符串
+             */
+            channel.queueBind(QUEUE_INFORM_EMAIL, EXCHANGE_TOPIC_INFORM, ROUTING_EMAIL);
+            channel.queueBind(QUEUE_INFORM_SMS, EXCHANGE_TOPIC_INFORM, ROUTING_SMS);
+
+            /**
+             * String exchange, String routingKey, AMQP.BasicProperties props, byte[] body
+             * exchange:交换机，不指定就使用默认
+             * routingKey:根据routingKey将消息转发到指定队列，默认交换机设为队列到名称
+             * props:额外的参数
+             * body:消息内容
+             */
+            for (int i = 0; i < 5; i++) {
+                String messageBody = "this is a email message";
+                channel.basicPublish(EXCHANGE_TOPIC_INFORM, "inform.email.sms", null, messageBody.getBytes());
+                System.out.println("发送成功:" + messageBody);
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        } catch (TimeoutException e) {
+            e.printStackTrace();
+        } finally {
+            try {
+                channel.close();
+                connection.close();
+            } catch (IOException e) {
+                e.printStackTrace();
+            } catch (TimeoutException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+}
